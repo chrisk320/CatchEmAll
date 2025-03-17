391ca1787e1502bc45a3e7cc30597e1bceadf3d8
Christian Kim <chris.kim320@gmail.com>
2025-03-16 22:07:39 -0400

    Ch 6.6 Prof Gallaugher SwiftUI Course

--- CatchEmAll/Creatures.swift
+++ CatchEmAll/Creatures.swift
@@ -11,7 +11,7 @@ import Foundation
 class Creatures {
     private struct Returned: Codable {
         var count: Int
-        var next: String //TODO: We want to change this to an optional
+        var next: String?
         var results: [Creature]
     }
     
@@ -36,9 +36,11 @@ class Creatures {
                 print("JSON ERROR: Could not decode returned JSON data")
                 return
             }
-            self.count = returned.count
-            self.urlString = returned.next
-            self.creaturesArray = returned.results
+            Task { @MainActor in
+                self.count = returned.count
+                self.urlString = returned.next ?? ""
+                self.creaturesArray = self.creaturesArray + returned.results
+            }
         } catch {
             print("ERROR: Could not get data from \(urlString)")
         }
