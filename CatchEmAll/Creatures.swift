0465a3f85324a41a49f65427aee31e5ba3340ff4
Christian Kim <chris.kim320@gmail.com>
2025-03-16 22:13:50 -0400

    Ch 6.7 Prof Gallaugher SwiftUI Course

--- CatchEmAll/Creatures.swift
+++ CatchEmAll/Creatures.swift
@@ -18,13 +18,16 @@ class Creatures {
     var urlString = "https://pokeapi.co/api/v2/pokemon"
     var count = 0
     var creaturesArray: [Creature] = []
+    var isLoading = false
     
     func getData() async {
         print("We are accessing the url \(urlString)")
+        isLoading = true
         
         // Create a URL
         guard let url = URL(string: urlString) else {
             print("ERROR: Could not create a URL from \(urlString)")
+            isLoading = false
             return
         }
         
@@ -34,15 +37,18 @@ class Creatures {
             //Try to decode JSON data into our own data structures
             guard let returned = try? JSONDecoder().decode(Returned.self, from: data) else {
                 print("JSON ERROR: Could not decode returned JSON data")
+                isLoading = false
                 return
             }
             Task { @MainActor in
                 self.count = returned.count
                 self.urlString = returned.next ?? ""
                 self.creaturesArray = self.creaturesArray + returned.results
+                isLoading = false
             }
         } catch {
             print("ERROR: Could not get data from \(urlString)")
+            isLoading = false
         }
     }
 }
